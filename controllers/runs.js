83b49a5352c240836bfd167af7953009244a9fa8
Adil Khan <adil_97@hotmail.it>
2026-01-21 13:13:16 +0000

    Add edit run functionality

--- controllers/runs.js
+++ controllers/runs.js
@@ -47,5 +47,38 @@ router.delete('/:runId', isSignedIn, async (req, res) => {
   }
 })
 
+router.get('/:runId/edit', isSignedIn, async (req, res) => {
+  try {
+    const user = await User.findById(req.session.user._id)
+    const run = user.runs.id(req.params.runId)
+
+    if (!run) return res.redirect('/runs')
+
+    return res.render('runs/edit', { run })
+  } catch (error) {
+    console.log(error)
+    return res.redirect('/runs')
+  }
+})
+
+router.put('/:runId', isSignedIn, async (req, res) => {
+  try {
+    const user = await User.findById(req.session.user._id)
+    const run = user.runs.id(req.params.runId)
+
+    if (!run) return res.redirect('/runs')
+
+    run.date = req.body.date
+    run.distance = Number(req.body.distance)
+    run.time = Number(req.body.time)
+
+    await user.save()
+    return res.redirect('/runs')
+  } catch (error) {
+    console.log(error)
+    return res.redirect('/runs')
+  }
+})
+
 
 module.exports = router
